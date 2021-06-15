a78d279ef75a2ab7919db87085425341daaf49e7
Ajaykavan <85919239+Ajaykavan@users.noreply.github.com>
2021-06-15 10:02:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["empCode"] = document.getElementById("empCode").value;
+    formData["salary"] = document.getElementById("salary").value;
+    formData["city"] = document.getElementById("city").value;
+    formData["age"] = document.getElementById("age").value;
+    formData["opinion"] = document.getElementById("opinion").value;
+    formData["dropdown"] = document.getElementById("dropdown").value;
+    formData["address"] = document.getElementById("address").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.empCode;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.salary;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.city;
+    cell5 = newRow.insertCell(4);
+    cell5.innerHTML = data.age;
+    cell6 = newRow.insertCell(5);
+    cell6.innerHTML = data.opinion;
+    cell7 = newRow.insertCell(6);
+    cell7.innerHTML = data.dropdown;
+    cell8 = newRow.insertCell(7);
+    cell8.innerHTML = data.address;
+    cell9 = newRow.insertCell(8);
+    cell9.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("fullName").value = "";
+    document.getElementById("empCode").value = "";
+    document.getElementById("salary").value = "";
+    document.getElementById("city").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("opinion").value = "";
+    document.getElementById("dropdown").value = "";
+    document.getElementById("address").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("empCode").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("city").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("age").value = selectedRow.cells[4].innerHTML;
+    document.getElementById("opinion").value = selectedRow.cells[5].innerHTML;
+    document.getElementById("dropdown").value = selectedRow.cells[6].innerHTML;
+    document.getElementById("address").value = selectedRow.cells[7].innerHTML;
+}
+
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.fullName;
+    selectedRow.cells[1].innerHTML = formData.empCode;
+    selectedRow.cells[2].innerHTML = formData.salary;
+    selectedRow.cells[3].innerHTML = formData.city;
+    selectedRow.cells[4].innerHTML = formData.age;
+    selectedRow.cells[5].innerHTML = formData.opinion;
+    selectedRow.cells[6].innerHTML = formData.dropdown;
+    selectedRow.cells[7].innerHTML = formData.address;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+function validate() {
+    isValid = true;
+    if (document.getElementById("fullName").value == "") {
+        isValid = false;
+        document.getElementById("fullNameValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
+            document.getElementById("fullNameValidationError").classList.add("hide");
+    }
+    return isValid;
+}
